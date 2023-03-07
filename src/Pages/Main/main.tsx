cfff55bc59567d8eb4ba0ef99f2fcbc7b645e8aa
olegpl1993 <olegpl1993@gmail.com>
2023-03-07 23:34:50 +0200

    update

--- src/Pages/Main/main.tsx
+++ src/Pages/Main/main.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAppSelector } from '../../hook';
 import './main.scss';
 
+// --------------------------------------------------------------
+interface TextKey {
+  textText: string;
+}
+interface Text {
+  [key: string]: TextKey;
+}
+const text: Text = {
+  ru: {
+    textText: 'Русский текст',
+  },
+  en: {
+    textText: 'English text',
+  },
+};
+// ------------------------------------------------------------------
+
 function Main() {
+  const [mainState, setMainState] = useState<string>('');
+  useEffect(() => {
+    setMainState('main');
+  }, []);
+  const lang: string = useAppSelector(
+    (state) => state.language.language,
+  );
   return (
     <div className="main">
-      main
+      <div className="title">
+        {mainState}
+      </div>
+      <div className="text">
+        {text[lang].textText}
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App/App';
+import { Provider } from 'react-redux';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import store from './store/store';
+import Header from './components/Header/header';
+import Main from './Pages/Main/main';
+import Second from './Pages/Second/second';
+import Next from './Pages/Next/next';
+import PageNotFound from './Pages/PageNotFound/pageNotFound';
+import Footer from './components/Footer/footer';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <App />,
+  <Provider store={store}>
+    <HashRouter>
+      <div className="wrapper">
+        <div className="headerBox"><Header /></div>
+        <div className="contentBox">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/second" element={<Second />} />
+            <Route path="/next" element={<Next />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
+        <div className="footerBox"><Footer /></div>
+      </div>
+    </HashRouter>
+  </Provider>,
 );
